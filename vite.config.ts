fac9ec3b161fa8b6cd0d73b76184fd2b8f38f143
Meowandcode <meowandcodedez@gmail.com>
2026-08-31 12:31:25 +0700

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const config = defineConfig({
+export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(), 
     tailwindcss(), 
-    tanstackStart(), 
+    tanstackStart({
+      server: {
+        preset: 'vercel',
+      },
+    }), 
     viteReact()
   ],
 });
-
-export default config;
